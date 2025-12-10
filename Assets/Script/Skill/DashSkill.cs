bb222818efe8387e7dd48bf739f9444f4d7547be
ImpureUknow <rachtanat.rati@mail.kmutt.ac.th>
2025-12-10 13:51:04 +0700

    Skill,Heal,Dash

---
+++ Assets/Script/Skill/DashSkill.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class DashSkill : SkillNAJA
+{
+    public float dashDistance = 3f;
+
+    public DashSkill()
+    {
+        skillName = "Dash";
+        cooldownTime = 3f;
+    }
+
+    public override void Activate(CharacterMovement player)
+    {
+        Vector3 dir = player.transform.forward;
+        player.transform.position += dir * dashDistance;
+        timer = 0f;
+    }
+
+    public override void UpdateSkill(CharacterMovement player)
+    {
+        
+    }
+}
